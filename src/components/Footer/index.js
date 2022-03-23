8ad28ffa4bbf5409c64d5d6f5b54aecef8c9ab6a
Air2air <todd@dunningmarketing.com>
2022-03-22 23:13:42 -0700

    Latest

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,29 +1,44 @@
 import {Link} from 'react-router-dom'
 import Image from 'react-bootstrap/Image'
-import logoMoasf from '../../images/site/moasf-logo.svg'
 import logoArms from '../../images/site/arms.png'
+import logoBlackhawk from '../../images/site/logo-blackhawk.svg'
+import logoMoasf from '../../images/site/moasf-logo.svg'
 import navLinks from '../../data/nav.json'
 import './styles.scss'
 
 const Footer = () => (
   <>
-    <div className="outer">
-      <div className="container footer-inner">
-        <Image src={logoArms} className="logo-footer" />
-        <div className="footer-links">
-          {navLinks.map(({id, link, name}) => (
-            <Link key={id} className="footer-link" to={link}>
-              {name}
-            </Link>
-          ))}
-        </div>
+    <div className="footer-links-outer">
+      <div className="container footer-links-inner">
+        {navLinks.map(({id, link, name}) => (
+          <Link key={id} className="footer-link" to={link}>
+            {name}
+          </Link>
+        ))}
+      </div>
+    </div>
+    <div className="footer-logos-outer">
+      <div className="container footer-logos-inner">
+        <Image src={logoArms} className="logo-footer" style={{opacity: 0.5}} />
+        <a
+          href="http://www.blackhawkmuseum.org/"
+          alt="The Blackhawk Museum"
+          target="top"
+          className="logo-link wide"
+        >
+          <Image src={logoBlackhawk} className="logo-footer wide" />
+        </a>
         <a
           href="http://www.moasf.com"
           alt="Mini Owners of America"
           target="top"
           className="logo-link"
         >
-          <Image src={logoMoasf} className="logo-footer" />
+          <Image
+            src={logoMoasf}
+            className="logo-footer"
+            style={{opacity: 0.5}}
+          />
         </a>
       </div>
     </div>
