100f3e9ad40cf6cb7b3f68108f1a50e1fe1d879a
Air2air <todd@dunningmarketing.com>
2022-03-23 22:37:30 -0700

    Works

--- src/components/Slideshow/index.js
+++ src/components/Slideshow/index.js
@@ -1,4 +1,5 @@
 import {useState, useEffect} from 'react'
+import Image from 'react-bootstrap/Image'
 import imageList from './imagesList.json'
 import './styles.scss'
 
@@ -7,18 +8,9 @@ const searchDir = '../../../public/images/photos/'
 const Slideshow = () => {
   const [activeImage, setActiveImage] = useState(0)
 
-  const randomImage = () => {
-    const randomPointer = Math.floor(Math.random() * imageList.length)
-    const imageUri = imageList
-    // return `${searchDir}${imageUri}`
-    return randomPointer
-  }
-
   useEffect(() => {
     const interval = setInterval(() => {
       const randomPointer = Math.floor(Math.random() * imageList.length)
-
-      console.log('This will run every second!')
       setActiveImage(randomPointer)
     }, 3000)
 
@@ -27,7 +19,11 @@ const Slideshow = () => {
 
   return (
     <div className="slideshow">
-      <img key={activeImage} src={activeImage} alt="doodle" />
+      <Image
+        src="../../../public/images/photos/DSC_8324.jpeg"
+        alt="slideshow"
+      />
+      <Image src={`${searchDir}${imageList[activeImage]}`} alt="doodle" />
     </div>
   )
 }
