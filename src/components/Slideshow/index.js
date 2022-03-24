2e8ef9c66409719956961c288b975ade5575a343
Air2air <todd@dunningmarketing.com>
2022-03-23 20:28:45 -0700

    useEffect and pointer

--- src/components/Slideshow/index.js
+++ src/components/Slideshow/index.js
@@ -1,20 +1,33 @@
+import {useState, useEffect} from 'react'
 import imageList from './imagesList.json'
 import './styles.scss'
 
 const searchDir = '../../../public/images/photos/'
 
 const Slideshow = () => {
+  const [activeImage, setActiveImage] = useState(0)
+
   const randomImage = () => {
-    const randomIndex = Math.floor(Math.random() * imageList.length)
+    const randomPointer = Math.floor(Math.random() * imageList.length)
     const imageUri = imageList
     // return `${searchDir}${imageUri}`
-    return randomIndex
+    return randomPointer
   }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const randomPointer = Math.floor(Math.random() * imageList.length)
+
+      console.log('This will run every second!')
+      setActiveImage(randomPointer)
+    }, 3000)
+
+    return () => clearInterval(interval)
+  }, [])
+
   return (
     <div className="slideshow">
-      {imageList.map((name, randomIndex) => (
-        <img key={name} src={randomImage(randomIndex)} alt="doodle" />
-      ))}
+      <img key={activeImage} src={activeImage} alt="doodle" />
     </div>
   )
 }
